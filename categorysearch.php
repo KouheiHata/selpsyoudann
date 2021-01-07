5a22e4deb54b0d9513df5a5603087e6616b3ec0f
shi-hatta <s_hatta@standtogether-fukushi.com>
2021-01-07 13:10:23 +0900

    検索枠作成

--- categorysearch.php
+++ categorysearch.php
@@ -9,10 +9,20 @@
      'orderby' => 'name',
      'hide_empty' => '1',
      'show_option_all' => '仕事一覧',
-     'value_field' => 'slug'
+     'value_field' => 'slug',
     );
     wp_dropdown_categories($args);
  ?>
+    <?php $tags = get_tags();?>
+    <?php if ( $tags ) ://複数検索の場合？ ?>
+        <select name='tag' id='tag'>
+        <option value="" selected="selected">地域選択</option>
+        <?php foreach ( $tags as $tag ): ?>
+        <option value="<?php echo esc_html( $tag->slug);  ?>"><?php echo esc_html( $tag->slug ); ?></option>
+        <?php echo $tag->slug;?>
+        <?php endforeach; ?>
+        </select>
+    <?php endif; ?>
  <input id="submit" type="submit" value="検索する" />
 </form>
 </div>
